17e74afb54ce6726af8e9435bf199b89950f0287
guaryc <93789303+guaryc@users.noreply.github.com>
2021-11-23 13:49:15 -0500

    Add files via upload

---
+++ Stock Analyzer App.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas
+import urllib
+import yfinance as yfinance
+from yfinance import Ticker
+value1 = input("Enter a ticker")
+import re
+from re import search
+pattern = value1
+re.search(r"\w\w\w\w+", pattern)
+if value1 == True:
+    print(value1)
+else:
+    print("We could not find that ticker")
+import pyparsing
+from datetime import datetime
+value2 = input("Enter a start date(mm/dd/yyyy)")
+value3 = input("Enter a end date(mm/dd/yyyy)")
+def datetime():
+    value2 = True
+if value2 == datetime:
+     print(value2)
+else:
+     print("Invalid date given")
+def datetime():
+     value3 = True
+if value3 == datetime:
+     print(value3)
+else:
+     print("Invalid date given")
